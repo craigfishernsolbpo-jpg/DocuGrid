a7497883e72375bf8be482f9272cbd6a34959f03
craigfishernsolbpo-jpg <craigfishernsolbpo@gmail.com>
2025-11-29 02:24:12 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,12 +21,37 @@
 //       }
 //     };
 // });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react()],
+
+    // required for vercel preview
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true
+    },
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
+      },
+    }
+  };
+});
+
